6eaa5768e2a6beb8fd7b46de1aea01c1f8bd0d15
mj9246N <mjfull603@gmail.com>
2026-08-03 22:50:21 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    // اضافه کردن کتابخانه‌ی coroutines (رفع کرش)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
